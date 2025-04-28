15aa8be8aea8f659e5499f51b91721abce4df0da
Pratik1joshi <jcprakash555@gmail.com>
2025-04-28 13:14:44 +0545

    basic cors updated

--- prediction_api.py
+++ prediction_api.py
@@ -43,7 +43,9 @@ app = FastAPI(title="LiveFire Prediction API",
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://wildfire-frontend-mp31.vercel.app", "https://wildfire-frontend-mp31-pr5f9a2ig-pratik-joshis-projects.vercel.app"  # Add this for local development
+        "https://wildfire-frontend-mp31.vercel.app", 
+        "https://wildfire-frontend-mp31-pr5f9a2ig-pratik-joshis-projects.vercel.app",
+        "https://wildfire-frontend-mp31-gw8o5s7lm-pratik-joshis-projects.vercel.app"  # Add this new URL
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],

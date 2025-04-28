35cb677a4e73539b201c025a92cab1a48af37375
Pratik1joshi <jcprakash555@gmail.com>
2025-04-28 15:01:00 +0545

    Cors heading improved

--- prediction_api.py
+++ prediction_api.py
@@ -50,7 +50,7 @@ app.add_middleware(
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allow_headers=["*"],
+    allow_headers=["Content-Type", "Authorization", "Accept", "Content-Disposition"],
     expose_headers=["Content-Type", "Authorization"],
 )
 
